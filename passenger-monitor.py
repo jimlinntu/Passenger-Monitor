86a574e5ec54438d3b4f682229d93318098e104d
NTU-CTLD <b04705003@ntu.edu.tw>
2019-06-12 11:35:52 +0800

    Revise passenger-monitor.py so that it can fit in crontab

--- passenger-monitor.py
+++ passenger-monitor.py
@@ -16,43 +16,37 @@ def sudo_passenger_status():
     xml_string = completed_process.stdout.decode("utf-8")
 
     return xml_string
+
 # If there is a cache file that is modified `CACHETIME ago, then just return the result in that cache file
-def get_xml_string_in_cache():
-      
-    xml_string = None 
+def get_cache():
+    canvas_get_wait_list_size = 0
     # Note: os.stat( * ) [8] == mtime(modified time)
     if os.path.isfile ( CACHEFILE ) and ( time.time() - os.stat ( CACHEFILE )[8]) < CACHETIME:
             # use cached data
             f = open ( CACHEFILE, 'r' )
-            xml_string = f.read()
+            canvas_get_wait_list_size = int(f.read())
             f.close()
     else:
             xml_string = sudo_passenger_status()
             # write file
             f = open ( CACHEFILE+'.TMP.'+ repr(os.getpid()), 'w' )
             print(os.getpid())
-            
-            f.write ( xml_string )
+            canvas_get_wait_list_size = parse_xml_string(xml_string)
+            f.write ( str(canvas_get_wait_list_size) )
             f.close()
             os.rename ( CACHEFILE+'.TMP.'+repr(os.getpid()), CACHEFILE ) # mv CACHE_FILE.TMP.pid CACHE_FILE.TMP
 
-    assert(xml_string is not None)
+    return canvas_get_wait_list_size
 
-    return xml_string
-
-
-
-def main():
-    xml_string = get_xml_string_in_cache()
+def parse_xml_string(xml_string):
     root = ET.fromstring(xml_string)
-    # Canvas requests in queue
-    canvas_get_wait_list_size = 0
 
     if DEBUG:
         import xml.dom.minidom
         dom = xml.dom.minidom.parseString(xml_string)
         print(dom.toprettyxml())
 
+    canvas_get_wait_list_size = 0
     for child in root:
         # [*] If there exist application group being processsed
         if child.tag == "supergroups":
@@ -62,7 +56,17 @@ def main():
                    canvas_group = supergroup.find("group")
                    assert(CANVAS_ABS_PATH in canvas_group.find("name").text)
                    canvas_get_wait_list_size = int(canvas_group.find("get_wait_list_size").text)
-    print(canvas_get_wait_list_size)
+
+    return canvas_get_wait_list_size
+
+def chmod_rx():
+    os.chmod(CACHEFILE, 0o555) # r_xr_xr_x
+
+def main():
+    # Canvas requests in queue
+    canvas_get_wait_list_size = get_cache()
+    # chmod so that snmp user can read
+    chmod_rx()
 
 if __name__ == "__main__":
     main()
