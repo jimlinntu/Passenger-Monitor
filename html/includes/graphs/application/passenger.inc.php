ebb3ee4ae437196b190234d1d33b3beba412fda5
NTU-CTLD <b04705003@ntu.edu.tw>
2019-06-13 16:38:47 +0800

    Finish passenger application graphing part

---
+++ html/includes/graphs/application/passenger.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+$scale_min = 0;
+
+require 'includes/graphs/common.inc.php';
+
+$passenger_rrd = rrd_name($device['hostname'], array('app', 'passenger', $app['app_id']));
+
+if (rrdtool_check_rrd_exists($passenger_rrd)) {
+    $rrd_filename = $passenger_rrd;
+}
+
+$ds = 'canvas_get_wait_list_size';
+
+$colour_area = 'CDEB8B';
+$colour_line = '006600';
+
+$colour_area_max = 'FFEE99';
+// boolean value (1 indicates true). For more information please see 'includes/graphs/generic_simplex.inc.php'
+
+$graph_max  = 1; 
+
+$unit_text = 'Wait list size';
+
+require 'includes/graphs/generic_simplex.inc.php';
